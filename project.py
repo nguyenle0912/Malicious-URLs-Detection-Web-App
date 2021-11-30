cbccae8b4a64f65b06a1137c207b93623986ca1d
theresale09 <phnguyen0912@gmail.com>
2021-11-29 23:30:17 -0600

    init commit

---
+++ project.py
@@ -0,0 +1,38 @@
+import pickle
+import streamlit as st
+import pandas as pd
+import numpy as np
+import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+
+# load the model from disk
+loaded_model = pickle.load(open('detect_mal_url_model.sav', 'rb'))
+# Using Tokenizer
+vectorizer = pickle.load(open('vectorizer.pk', 'rb'))
+
+# defining the function which will make the prediction using the data which the user inputs 
+def prediction(user_url):   
+    vectorized_user_url = vectorizer.transform([user_url])
+    prediction = loaded_model.predict(vectorized_user_url)
+    return prediction[0]
+
+# this is the main function in which we define our webpage  
+def main():
+    # front end elements of the web page 
+    html_temp = """ 
+    <div style ="background-color:white;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Malicious URL Detector</h1> 
+    </div> 
+    """
+    # display the front end aspect
+    st.markdown(html_temp, unsafe_allow_html = True)
+    st.markdown("***")
+    user_url = st.text_input("Enter the suspicious url here: ")
+    if user_url:
+        
+        st.write(prediction(user_url))
+    
+if __name__ == '__main__':
+    main()
